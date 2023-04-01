0dfc8003a0cd135742dd535be8b8082c1e67b2c2
Alan Barucq <129017523+Barucq@users.noreply.github.com>
2023-04-01 16:16:22 -0600

    Update user.js

--- models/user.js
+++ models/user.js
@@ -23,6 +23,10 @@ const UserSchema = new Schema ({
         type: String,
         required: true 
     },
+    img: {
+        type: String,
+        require: true
+    },
     active: {
         type: Boolean,
         required: true
